a6fb5b7ff73c49900ba5cc2259717b7d7a2776d0
Fernanda Fernandes <98698976+Ferfkm@users.noreply.github.com>
2023-10-24 19:07:29 -0300

    audio

--- main.js
+++ main.js
@@ -6,8 +6,10 @@ const listadeTeclas = document.querySelectorAll(".tecla");
 
 let contador = 0;
 while(contador < listadeTeclas.length){
+    const efeito = listadeTeclas[contador].classList[1];
+    const idAudio = "#som_"+efeito;
     listadeTeclas[contador].onclick = function(){
-        tocaSom("som_tecla_aplausos");
+        tocaSom(idAudio)
     }
     contador = contador + 1;
     console.log(contador);
