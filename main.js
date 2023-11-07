023f7d8621dc209dce1a8f5b357badc6ac16f267
Fernanda Fernandes <98698976+Ferfkm@users.noreply.github.com>
2023-11-07 19:10:24 -0300

    for

--- main.js
+++ main.js
@@ -4,15 +4,14 @@ function tocaSom(idElementoAudio){
 }
 const listadeTeclas = document.querySelectorAll(".tecla");
 
-let contador = 0;
-while(contador < listadeTeclas.length){
-    const efeito = listadeTeclas[contador].classList[1];
-    const idAudio = "#som_"+efeito;
-    listadeTeclas[contador].onclick = function(){
+
+for(let contador = 0; contador < listadeTeclas.length; contador++){
+    const tecla = listadeTeclas[contador];
+    const efeito = tecla.classList[1];
+    const idAudio = `#som_${efeito}`; 
+    tecla.onclick = function(){
         tocaSom(idAudio)
     }
     contador = contador + 1;
-    console.log(contador);
 }
-
 
